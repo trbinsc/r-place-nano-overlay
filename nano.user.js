0c857b38f5b836287dddd1242d5e0810509f71b4
Thomas <31908601+trb0023@users.noreply.github.com>
2022-04-01 20:11:42 -0500

    change

--- nano.user.js
+++ nano.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name         r/place SpaceX Template
+// @name         r/place Nano Template
 // @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  Template for spacex graphic
-// @author       /u/fiercedude
+// @version      0.1
+// @description  Template for nano graphic
+// @author       /u/trbinsc
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
-// @updateURL    https://github.com/fiercedude/r-place-spacex-overlay/raw/main/spacex.user.js
-// @downloadURL  https://github.com/fiercedude/r-place-spacex-overlay/raw/main/spacex.user.js
+// @updateURL    https://github.com/trbinsc/r-place-nano-overlay/raw/main/snano.user.js
+// @downloadURL  https://github.com/trbinsc/r-place-nano-overlay/raw/main/nano.user.js
 // @grant        none
 // ==/UserScript==
 
@@ -16,7 +16,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://cdn.discordapp.com/attachments/502460438425108490/959610748248666152/trans_spacex_logo_v2.png";
+            i.src = "https://media.discordapp.net/attachments/912947691661692929/959620249202880532/trans_nano_logo.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
             console.log(i);
             return i;
