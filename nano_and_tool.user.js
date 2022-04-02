4de3082a2a471b49cb69dd467c366cbacc795caf
Thomas <31908601+trb0023@users.noreply.github.com>
2022-04-01 20:46:29 -0500

    Create nano_and_tool.user.js

---
+++ nano_and_tool.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         r/place Nano Template
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Template for nano graphic
+// @author       /u/trbinsc
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @updateURL    https://github.com/trbinsc/r-place-nano-overlay/raw/main/snano.user.js
+// @downloadURL  https://github.com/trbinsc/r-place-nano-overlay/raw/main/nano.user.js
+// @grant        none
+// ==/UserScript==
+
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://cdn.discordapp.com/attachments/912947691661692929/959629170634268692/trans_nano_logo.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+
+}
